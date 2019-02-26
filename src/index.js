ae6f3ec7baaee6da9580f7611a569e7772ecda43
Denis <denis360_93@mail.ru>
2019-02-26 23:47:13 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  return removeString(str, bracketsConfig);
+}
+
+function removeString(removbleStr,template) {
+  var result = false;
+
+  if (removbleStr.length > 0) {
+    const checker = arrayContainString(removbleStr,template);
+    if (checker.contains) {
+      result = removeString(checker.removbleStr,template);
+    }
+  } else {
+    result = true;
+  }
+
+  return result;
+}
+
+function arrayContainString(str, array) {
+  var contains = false;
+  var removbleStr = str;
+
+  array.forEach(function(arr) {
+    const template = arr.join('');
+    if (str.includes(template)) {
+      removbleStr = removbleStr.replace(template, "");
+      contains = true;
+    }
+  })
+
+  return {contains,removbleStr};
 }
